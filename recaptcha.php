c6c0a20b178408f722b44c0a97d97b19e5d9611a
isaacleesang <32001782+isaacleesang@users.noreply.github.com>
2017-09-16 10:05:16 +1200

    Update recaptcha.php

--- recaptcha.php
+++ recaptcha.php
@@ -18,15 +18,13 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Google reCAPTCHA</title>
+    <title>reCAPTCHA</title>
 </head>
 
 <body>
     <br><br>
     <form action="" method="post">
-        <input type="text" name="myreq"><br>
         <div class="g-recaptcha" data-sitekey="6LfE5hQTAAAAAH2Aj5lMPSA4yUQzsEDGxoJbV4Ib"></div>
-        <input type="submit" name="submit" value="Send Request!">
     </form>
 </body>
 <script src='https://www.google.com/recaptcha/api.js'></script>
